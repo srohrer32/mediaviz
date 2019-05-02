906348ad5a184dbc3dae287916c0d1991ec7129f
Samuel Rohrer <srohrer32@gmail.com>
2019-05-02 09:43:32 -0700

    display matplotlib object in webgui through container

--- webgui/app.py
+++ webgui/app.py
@@ -2,12 +2,36 @@
 # Python app for simple webpgae
 #
 
-from flask import Flask, render_template
+import io
+import random
+from flask import Flask, render_template, Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 app = Flask(__name__)
 
+# Simple hello world test
 @app.route('/')
 def hello():
     return render_template('home.html')
 
+# try to display a figure
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
+
+
+# run the application
 if __name__ == "__main__":
     app.run()
