9474f0ea13d63c0e58e58c09006f097182ea9558
Samuel Rohrer <srohrer32@gmail.com>
2020-08-11 15:58:32 -0400

    rev python package version for new dependencies

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 # make the package
 setup(name='mediaviz',
-      version='1.0',
+      version='1.1',
       description='New media (film, tv, music) recommendation system with Web GUI',
       author='Samuel Rohrer',
       url='https://srohrer32.github.io',
